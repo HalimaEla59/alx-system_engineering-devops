b1da851609a8902867cb29434b76c931cfcb9a64
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2024-01-03 00:32:08 +0100

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""returns information about TODO list progress for a given employee ID."""
+import requests
+import sys
+
+if __name__ == "__main__":
+  if len(argv) == 2:
+    APIurl = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(APIurl + "users/{}".format(sys.argv[1])).json()
+    ToDo = requests.get(APIurl + "ToDo", params={"userId": sys.argv[1]}).json()
+    completedToDo = [t.get("title") for t in ToDo if t.get("completedToDo") is True]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completedToDo), len(ToDo)))
+    [print("\t {}".format(i)) for i in completedToDo]
