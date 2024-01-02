44ad76a4bb9f5fa387f54e1b3320a59a458b546f
HalimaEla59 <halimaelabbadi@gmail.com>
2024-01-02 15:40:56 -0800

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,12 +4,12 @@ import requests
 import sys
 
 if __name__ == "__main__":
-  if len(argv) == 2:
-    APIurl = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(APIurl + "users/{}".format(sys.argv[1])).json()
-    ToDo = requests.get(APIurl + "ToDo", params={"userId": sys.argv[1]}).json()
-    completedToDo = [t.get("title") for t in ToDo if t.get("completedToDo") is True]
+    if len(sys.argv) == 2:
+        APIurl = "https://jsonplaceholder.typicode.com/"
+        user = requests.get(APIurl + "users/{}".format(sys.argv[1])).json()
+        ToDo = requests.get(APIurl + "ToDo", params={"userId": sys.argv[1]}).json()
+        completedToDo = [t.get("title") for t in ToDo if t.get("completedToDo") is True]
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completedToDo), len(ToDo)))
-    [print("\t {}".format(i)) for i in completedToDo]
+        print("Employee {} is done with tasks({}/{}):".format(
+            user.get("name"), len(completedToDo), len(ToDo)))
+        [print("\t {}".format(i)) for i in completedToDo]
