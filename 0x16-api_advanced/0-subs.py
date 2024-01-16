b1bf13ae3d7355b3234ccb2b428d0a81f19f9346
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2024-01-17 00:23:53 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """queries the Reddit API and returns the number of subscribers for a given subreddit."""
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
     """If an invalid subreddit is given, the function should return 0"""
-    link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(link, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    header = {'User-Agent': 'Illustrious_Event_19'}
+    SubNum = get('https://www.reddit.com/r/{}/about.json'.format(
+        subreddit), headers=header).json()
+    try:
+        return SubNum.get('data').get('subscribers')
+    except:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
