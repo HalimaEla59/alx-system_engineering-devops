dcb6068749e912d3714d82274ff8ce3caa2d209c
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2024-01-17 00:29:08 +0100

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit"""
+from requests import get
+from sys import argv
+
+
+def top_ten(subreddit):
+    """If not a valid subreddit, print None"""
+    header = {'User-Agent': 'Dan Kazam'}
+    try:
+        counter = get('https://www.reddit.com/r/{}/hot.json?count=10'.format(
+            subreddit), headers=header).json().get('data').get('children')
+        print('\n'.join([dic.get('data').get('title')
+                         for dic in counter][:10]))
+    except:
+        print('None')
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
