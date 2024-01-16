5f663fae5d9c3aaff5492a1ac129282e5e31ebcd
HalimaEla59 <halimaelabbadi@gmail.com>
2024-01-16 15:34:47 -0800

    merge

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit"""
+"""queries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit"""
 from requests import get
 from sys import argv
 
