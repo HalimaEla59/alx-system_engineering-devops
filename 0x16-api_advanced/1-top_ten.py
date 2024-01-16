312a4d4194f4c3b9809824a23e698ce3d698ae28
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2024-01-17 00:32:54 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit"""
+"""queries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit"""
 from requests import get
 from sys import argv
 
 
 def top_ten(subreddit):
     """If not a valid subreddit, print None"""
-    header = {'User-Agent': 'Dan Kazam'}
+    header = {'User-Agent': 'Illustrious_Event_19'}
     try:
         counter = get('https://www.reddit.com/r/{}/hot.json?count=10'.format(
             subreddit), headers=header).json().get('data').get('children')
